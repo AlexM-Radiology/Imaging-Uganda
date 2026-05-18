f9d032f14ff6d5863787a3dac0e8ad5e8f3d9999
Mubajje Alex <alexmradiology@gmail.com>
2026-05-19 00:25:44 +0300

    Created the main articles page and added a sample article about pelvic inflammatory disease

--- src/content.config.ts
+++ src/content.config.ts
@@ -17,4 +17,19 @@ const articles = defineCollection({
 		}),
 });
 
-export const collections = { articles };
+const cases = defineCollection({
+	// Load Markdown and MDX files in the `src/content/cases/` directory.
+	loader: glob({ base: './src/content/cases', pattern: '**/*.{md,mdx}' }),
+	// Type-check frontmatter using a schema
+	schema: ({ image }) =>
+		z.object({
+			title: z.string(),
+			description: z.string(),
+			// Transform string to Date object
+			pubDate: z.coerce.date(),
+			updatedDate: z.coerce.date().optional(),
+			heroImage: z.optional(image()),
+		}),
+});
+
+export const collections = { articles, cases };
